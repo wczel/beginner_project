2934d14e5d994028992d3511d61f68ff3a5e3da9
Jakub Wczelik <jakub.wczelik@aptiv.com>
2023-08-29 15:06:40 +0000

    initial commit

---
+++ languages/tests.py
@@ -0,0 +1,68 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+
+class HomepageTests(SimpleTestCase):
+    def test_url_by_pattern(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_template(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_for_title(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Programming Languages</h1>")
+
+    def test_for_cards(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, '<div class="card">', count=3)
+
+
+class AboutpageTests(SimpleTestCase):
+    def test_url_by_pattern(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_for_title(self):
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>About Page</h1>")
+
+    def test_for_content(self):
+        response = self.client.get(reverse("about"))
+        self.assertContains(
+            response,
+            "<p>This page is an exercise in learning how Django displays templates through URL pattern matching.</p>",
+        )
+
+
+class PythonpageTests(SimpleTestCase):
+    def test_url_by_pattern(self):
+        response = self.client.get("/python/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_for_title(self):
+        response = self.client.get(reverse("python"))
+        self.assertContains(response, "<h1>Python</h1>")
+
+    def test_for_subtitles(self):
+        response = self.client.get(reverse("python"))
+        self.assertContains(response, "<h2>History</h2>")
+        self.assertContains(response, "<h2>Uses</h2>")
+
+
+class FsharppageTests(SimpleTestCase):
+    def test_url_by_pattern(self):
+        response = self.client.get("/fsharp/")
+        self.assertEqual(response.status_code, 200)
+
+
+class RacketpageTests(SimpleTestCase):
+    def test_url_by_pattern(self):
+        response = self.client.get("/racket/")
+        self.assertEqual(response.status_code, 200)
